7120596a7b8e4170ebf395f9269f4bd4a3124965
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-04-06 10:54:46 +0530

    adding the new row using form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import "./App.css";
 import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import "./App.css";
 
 const studentStaticData = [
   {
@@ -17,6 +18,33 @@ const studentStaticData = [
 const App = () => {
   const [studentList, setStudentList] = useState(studentStaticData);
 
+  const [enteredStudentName, setEnteredStudentName] = useState("");
+  const [enteredUniversity, setEnteredUniversity] = useState("");
+
+  const handleStudentNameChange = (event) => {
+    setEnteredStudentName(event.target.value);
+  };
+  const handleUniversityChange = (event) => {
+    setEnteredUniversity(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {    
+    event.preventDefault();
+
+    const studentFormData = {
+      id: nanoid(),
+      studentName: enteredStudentName,
+      university: enteredUniversity,
+    };
+
+    setEnteredStudentName("");
+    setEnteredUniversity("");
+
+    const newStudentFormData = [...studentList, studentFormData];
+    setStudentList(newStudentFormData);
+    console.log(studentFormData); 
+  };
+
   return (
     <div className="app-container">
       <h1>React-Student App-Practice2</h1>
@@ -30,13 +58,33 @@ const App = () => {
         </thead>
         <tbody>
           {studentList.map((student) => (
-            <tr>
+            <tr key={student.id}>
               <td>{student.studentName}</td>
-              <td>{student.university}</td> 
+              <td>{student.university}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      <h2>Add Student Details</h2>
+      <form onSubmit={formSubmitHandler}>
+        <input
+          type="text"
+          name="studentName"
+          required="required"
+          placeholder="Enter Student Name"
+          value={enteredStudentName}
+          onChange={handleStudentNameChange}
+        />
+        <input
+          type="text"
+          name="university"
+          required="required"
+          placeholder="Enter University Name"
+          value={enteredUniversity}
+          onChange={handleUniversityChange}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
